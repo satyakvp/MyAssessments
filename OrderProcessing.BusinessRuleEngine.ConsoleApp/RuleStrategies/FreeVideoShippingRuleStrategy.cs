b7abfec442267e2bf9b33c11562cf3afe2973d87
Vara Prasad Kalapu <vprasad.kalapu@gmail.com>
2021-10-06 18:35:44 +0100

    FreeVideoShippingRuleStrategyTests added along with strategy class

---
+++ OrderProcessing.BusinessRuleEngine.ConsoleApp/RuleStrategies/FreeVideoShippingRuleStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrderProcessing.BusinessRuleEngine.ConsoleApp.UnitTests
+{
+    public class FreeVideoShippingRuleStrategy
+    {
+        public bool IsMatch(PaymentType type)
+        {
+            return type == PaymentType.LearningToSkiVideo;
+        }
+    }
+}
