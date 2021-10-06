b9301180b3ef388d58a1c9e7989f1358e2639660
Vara Prasad Kalapu <vprasad.kalapu@gmail.com>
2021-10-06 19:02:41 +0100

    Strategy pattern applied here. This registry would return respective strategy when its matched. ChainOfResponsibitlity Pattern can also be applied here in place of Strategy. But I prefer to go for Strategy pattern in this case.

---
+++ OrderProcessing.BusinessRuleEngine.ConsoleApp.UnitTests/BusinessRuleStrategyRegistryTests.cs
@@ -0,0 +1,123 @@
+﻿using FluentAssertions;
+using OrderProcessing.BusinessRuleEngine.ConsoleApp.Constants;
+using OrderProcessing.BusinessRuleEngine.ConsoleApp.Factory;
+using OrderProcessing.BusinessRuleEngine.ConsoleApp.RuleStrategies.Definitions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace OrderProcessing.BusinessRuleEngine.ConsoleApp.UnitTests
+{
+    public class BusinessRuleStrategyRegistryTests
+    {
+        private BusinessRuleStrategyRegistry businessRuleStrategyRegistry;
+
+        public BusinessRuleStrategyRegistryTests()
+        {
+            // BusinessRuleStrategyRegistry shoud be mocked by using Mocking frameworks like Moq etc.
+            this.businessRuleStrategyRegistry = new BusinessRuleStrategyRegistry(
+                            new List<IBusinessRuleStrategy>()
+                                    {
+                                        new ShippingRuleStrategy(),
+                                        new RoyaltyDepartmentStrategy(),
+                                        new ActiviateMembershipStrategy(),
+                                        new UpgradeMembershipStrategy(),
+                                        new ActivateOrUpgradeMembershipStrategy(),
+                                        new FreeVideoShippingRuleStrategy(),
+                                        new CommissionPaymentRuleStrategy()
+                                    }
+                            );
+        }
+
+        [Fact]
+        public void ShouldStrategyRegistryReturnCorrectStrategyWhenPaymentForActiveMembership()
+        {
+            // Arrange
+
+            //Act
+            var strategy = this.businessRuleStrategyRegistry.GetBusinessRuleStrategy(PaymentType.ActivateMembership);
+
+            // Assert
+            strategy.Should().BeOfType<ActiviateMembershipStrategy>();
+            strategy.HandleOrder().Should().Be(StrategyConstants.ActivateMemberText);
+        }
+
+        [Fact]
+        public void ShouldStrategyRegistryReturnCorrectStrategyWhenPaymentForUpgradeMembership()
+        {
+            // Arrange
+
+            //Act
+            var strategy = this.businessRuleStrategyRegistry.GetBusinessRuleStrategy(PaymentType.UpgradeMembership);
+
+            // Assert
+            strategy.Should().BeOfType<UpgradeMembershipStrategy>();
+            strategy.HandleOrder().Should().Be(StrategyConstants.UpgradeMemershipText);
+        }
+
+        [Fact]
+        public void ShouldStrategyRegistryReturnCorrectStrategyWhenPaymentForActiveOrUpgradeMembership()
+        {
+            // Arrange
+
+            //Act
+            var strategy = this.businessRuleStrategyRegistry.GetBusinessRuleStrategy(PaymentType.ActivateOrUpgradeMembership);
+
+            // Assert
+            strategy.Should().BeOfType<ActivateOrUpgradeMembershipStrategy>();
+            strategy.HandleOrder().Should().Be(StrategyConstants.ActivateOrUpgradeMembershipText);
+        }
+
+        [Fact]
+        public void ShouldStrategyRegistryReturnCorrectStrategyWhenPaymentForLearningToSkiVideo()
+        {
+            // Arrange
+
+            //Act
+            var strategy = this.businessRuleStrategyRegistry.GetBusinessRuleStrategy(PaymentType.LearningToSkiVideo);
+
+            // Assert
+            strategy.Should().BeOfType<FreeVideoShippingRuleStrategy>();
+            strategy.HandleOrder().Should().Be(StrategyConstants.FreeVideoShippingText);
+        }
+
+        [Fact]
+        public void ShouldStrategyRegistryReturnCorrectStrategyWhenPaymentForBook()
+        {
+            // Arrange
+
+            //Act
+            var strategy = this.businessRuleStrategyRegistry.GetBusinessRuleStrategy(PaymentType.Book);
+
+            // Assert
+            strategy.Should().BeOfType<RoyaltyDepartmentStrategy>();
+            strategy.HandleOrder().Should().Be(StrategyConstants.RoyaltyDepartmentText);
+        }
+
+        [Fact]
+        public void ShouldStrategyRegistryReturnCorrectStrategyWhenPaymentForPhysical()
+        {
+            // Arrange
+
+            //Act
+            var strategy = this.businessRuleStrategyRegistry.GetBusinessRuleStrategy(PaymentType.Physical);
+
+            // Assert
+            strategy.Should().BeOfType<ShippingRuleStrategy>();
+            strategy.HandleOrder().Should().Be(StrategyConstants.ShippingRuleText);
+        }
+
+        [Fact]
+        public void ShouldStrategyRegistryReturnCorrectStrategyWhenPaymentForBookOrPhysical()
+        {
+            // Arrange
+
+            //Act
+            var strategy = this.businessRuleStrategyRegistry.GetBusinessRuleStrategy(PaymentType.PhysicalOrBook);
+
+            // Assert
+            strategy.Should().BeOfType<CommissionPaymentRuleStrategy>();
+            strategy.HandleOrder().Should().Be(StrategyConstants.CommissionPaymentRuleText);
+        }
+
+    }
+}
---
+++ OrderProcessing.BusinessRuleEngine.ConsoleApp/Factory/BusinessRuleStrategyRegistry.cs
@@ -0,0 +1,32 @@
+﻿using OrderProcessing.BusinessRuleEngine.ConsoleApp.RuleStrategies.Definitions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OrderProcessing.BusinessRuleEngine.ConsoleApp.Factory
+{
+    /// <summary>
+    /// Strategy pattern applied here. This registry would return
+    /// respective strategy when its matched.
+    /// ChainOfResponsibitlity Pattern can also be applied here in place of Strategy. 
+    /// But I prefer to go for Strategy pattern.
+    /// </summary
+    public sealed class BusinessRuleStrategyRegistry
+    {
+        private readonly IEnumerable<IBusinessRuleStrategy> businessRuleStrategies;
+
+        /// <summary>
+        /// Dependancy Inversion principle applied
+        /// </summary>
+        /// <param name="businessRuleStrategies"></param>
+        public BusinessRuleStrategyRegistry(IEnumerable<IBusinessRuleStrategy> businessRuleStrategies)
+        {
+            this.businessRuleStrategies = businessRuleStrategies;
+        }
+
+        public IBusinessRuleStrategy GetBusinessRuleStrategy(PaymentType productType)
+        {
+            return businessRuleStrategies.FirstOrDefault(p => p.IsMatch(productType));
+        }
+
+    }
+}
