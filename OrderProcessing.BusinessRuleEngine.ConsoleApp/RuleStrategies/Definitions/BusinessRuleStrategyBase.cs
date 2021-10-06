cd4eaf45593a7820b3c86dd65fbf9acbc76d2bdc
Vara Prasad Kalapu <vprasad.kalapu@gmail.com>
2021-10-06 18:44:23 +0100

    Added "BusinessRuleStrategyBase" to have common behaviours across all the strategies so that all strategies can directly inherit common funtionality from this base class which implments interface.

---
+++ OrderProcessing.BusinessRuleEngine.ConsoleApp/RuleStrategies/Definitions/BusinessRuleStrategyBase.cs
@@ -0,0 +1,13 @@
+﻿namespace OrderProcessing.BusinessRuleEngine.ConsoleApp.RuleStrategies.Definitions
+{
+    /// <summary>
+    /// BusinessRuleStrategyBase which holds common behaviour which can inherited 
+    /// into all the respective strategies.
+    /// </summary>
+    public abstract class BusinessRuleStrategyBase : IBusinessRuleStrategy
+    {
+        public abstract bool IsMatch(PaymentType type);
+
+        public abstract string HandleOrder();
+    }
+}
