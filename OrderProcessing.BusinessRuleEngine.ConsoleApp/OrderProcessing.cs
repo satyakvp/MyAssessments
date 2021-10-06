59412cebc6be92ad61d8276dcee4b4ae3da445e0
Vara Prasad Kalapu <vprasad.kalapu@gmail.com>
2021-10-06 19:33:01 +0100

    OrderProcessing: Installed Extensions.DependencyInjections to inject strategy dependencies from external source.

--- OrderProcessing.BusinessRuleEngine.ConsoleApp/OrderProcessing.cs
+++ OrderProcessing.BusinessRuleEngine.ConsoleApp/OrderProcessing.cs
@@ -1,12 +1,63 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using OrderProcessing.BusinessRuleEngine.ConsoleApp.Factory;
+using OrderProcessing.BusinessRuleEngine.ConsoleApp.RuleStrategies.Definitions;
+using System;
+using System.Collections.Generic;
 
 namespace OrderProcessing.BusinessRuleEngine.ConsoleApp
 {
     public class OrderProcessing
     {
-        public static void Main(string[] args)
+        private static IServiceProvider _serviceProvider;
+
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                // Registered required services in Dependency Injection Container.
+                RegisterServices();
+
+                OrderProcessingEngine();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private static void RegisterServices()
+        {
+            var serviceCollection = new ServiceCollection()
+                .AddScoped(_ =>
+                {
+                    return new BusinessRuleStrategyRegistry
+                    (
+                        new List<IBusinessRuleStrategy>()
+                        {
+                            new ShippingRuleStrategy(),
+                            new RoyaltyDepartmentStrategy(),
+                            new ActiviateMembershipStrategy(),
+                            new UpgradeMembershipStrategy(),
+                            new ActivateOrUpgradeMembershipStrategy(),
+                            new FreeVideoShippingRuleStrategy(),
+                            new CommissionPaymentRuleStrategy()
+                        }
+                    );
+                });
+
+            serviceCollection.AddScoped<OrderProcessingRuleEngine, OrderProcessingRuleEngine>();
+
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        private static void OrderProcessingEngine()
+        {
+            var orderProcessingRuleEngine = _serviceProvider.GetService<OrderProcessingRuleEngine>();
+
+            var toBePrint = orderProcessingRuleEngine.ProcessPayment(PaymentType.ActivateMembership);
+
+            Console.WriteLine(toBePrint);
+            Console.ReadLine();
         }
     }
 }
