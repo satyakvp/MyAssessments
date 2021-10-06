1b1350718c063ff3cf0b4cee2e799b1e23b51204
Vara Prasad Kalapu <vprasad.kalapu@gmail.com>
2021-10-06 17:50:48 +0100

    Initial - Unittest project created

---
+++ OrderProcessing.BusinessRuleEngine.ConsoleApp.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace OrderProcessing.BusinessRuleEngine.ConsoleApp.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
